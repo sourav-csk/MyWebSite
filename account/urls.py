32a1eeee614a370dd1744da17284f3f3618e8afc
unknown <yunkaijiang@live.com>
2018-06-12 22:50:34 +0800

    2018.6.12

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'login/^$', views.user_login, name="userlogin"),
+    url(r'login/$', views.user_login, name="userlogin"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+from account.froms import LoginForm
 # Create your views here.
 
 def user_login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd["username"], passwprd=cd["password"])
+            if user:
+                login(request, user)
+                return HttpResponse("Welcome")
+            else:
+                return HttpResponse("Sorry")
+        else:
+            return HttpResponse("Invalid login")
+
+    if request.method == "GET":
+        login_form = LoginForm()
+        return render(request, "account/login.html", {"form": login_form})
